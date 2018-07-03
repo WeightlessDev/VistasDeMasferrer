fb219b8d959e09688c541d6aa2501cc18bd286c5
Denis Garcia <dev@weightlessgames.com>
2018-07-02 21:41:59 -0600

    Start scene with hidden list

--- index.js
+++ index.js
@@ -133,7 +133,7 @@
 
   // Start with the scene list open on desktop.
   if (!document.body.classList.contains('mobile')) {
-    showSceneList();
+    hideSceneList();
   }
 
   // Set handler for scene switch.
